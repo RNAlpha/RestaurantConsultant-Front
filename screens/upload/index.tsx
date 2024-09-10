a1d7180f69c02e75f8634fed7998234e34ec506d
Taehyeungkim <kth1837@snu.ac.kr>
2024-09-11 00:48:42 +0900

    :sparkles: feat: added prompt message input

--- screens/upload/index.tsx
+++ screens/upload/index.tsx
@@ -3,6 +3,10 @@ import {
   TouchableOpacity,
   Text,
   useWindowDimensions,
+  TextInput,
+  KeyboardAvoidingView,
+  Keyboard,
+  Pressable,
 } from "react-native";
 import style from "./style";
 import Fontisto from "@expo/vector-icons/Fontisto";
@@ -11,73 +15,129 @@ import {
   BottomSheetModalProvider,
   BottomSheetView,
 } from "@gorhom/bottom-sheet";
-import { useCallback, useMemo, useRef } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import Feather from "@expo/vector-icons/Feather";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Entypo from "@expo/vector-icons/Entypo";
 import * as ImagePicker from "expo-image-picker";
+import { Image } from "expo-image";
+import { ASSETS } from "../../assets/assets";
 
 export default function UploadScreen() {
   const { width } = useWindowDimensions();
 
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
+  const [image, setImage] = useState<string | null>(null);
+  const imageOption: ImagePicker.ImagePickerOptions = useMemo(() => {
+    return {
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+      aspect: [3, 4],
+    };
+  }, []);
+
   const bottomSheetPresent = useCallback(() => {
     bottomSheetModalRef.current?.present();
+  }, []);
+
+  const bottomSheetDismiss = useCallback(() => {
+    bottomSheetModalRef.current?.dismiss();
   }, []);
 
   const snapPoints = useMemo(() => ["45%", "50%"], []);
 
   const openImageLibrary = async () => {
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    });
+    const result = await ImagePicker.launchImageLibraryAsync(imageOption);
+    result.assets && setImage(result.assets[0].uri);
+    bottomSheetDismiss();
   };
 
   const openCamera = async () => {
     const permission = await ImagePicker.requestCameraPermissionsAsync();
 
     if (permission.granted) {
-      const result = await ImagePicker.launchCameraAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      });
+      const result = await ImagePicker.launchCameraAsync(imageOption);
+      result.assets && setImage(result.assets[0].uri);
+      bottomSheetDismiss();
     }
   };
 
   return (
     <BottomSheetModalProvider>
-      <View style={style.container}>
-        <TouchableOpacity
-          style={style["upload-button"]}
-          onPress={bottomSheetPresent}
-        >
-          <Fontisto name="photograph" size={width * 0.3} color="green" />
-          <Text
-            style={[style["upload-button-text"], { fontSize: width * 0.05 }]}
-          >
-            매장의 사진을 추가해보세요!
-          </Text>
-        </TouchableOpacity>
-        <BottomSheetModal ref={bottomSheetModalRef} snapPoints={snapPoints}>
-          <BottomSheetView style={[style["bottom-sheet-view"]]}>
+      <Pressable style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
+        <KeyboardAvoidingView style={style.container} behavior="height">
+          <View style={style["upload-preview-container"]}>
             <TouchableOpacity
-              style={style["bottom-sheet-button"]}
-              onPress={() => openCamera()}
-            >
-              <Feather name="camera" size={width * 0.2} color="black" />
-              <Text>사진을 찍어보세요!</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={style["bottom-sheet-button"]}
+              style={style["upload-preview"]}
               onPress={() => {
-                openImageLibrary();
+                Keyboard.dismiss();
+                bottomSheetPresent();
               }}
             >
-              <FontAwesome name="photo" size={width * 0.2} color="black" />
-              <Text>사진을 선택하세요!</Text>
+              {!image ? (
+                <>
+                  <Fontisto
+                    name="photograph"
+                    size={width * 0.3}
+                    color="green"
+                  />
+                  <Text
+                    style={[
+                      style["upload-button-text"],
+                      { fontSize: width * 0.05 },
+                    ]}
+                  >
+                    매장의 사진을 추가해보세요!
+                  </Text>
+                </>
+              ) : (
+                <Image
+                  source={{ uri: image }}
+                  style={style["image-preview"]}
+                  contentFit="cover"
+                />
+              )}
             </TouchableOpacity>
-          </BottomSheetView>
-        </BottomSheetModal>
-      </View>
+          </View>
+          <View style={style["image-prompt-container"]}>
+            <TextInput
+              style={style["image-prompt"]}
+              placeholder="AI에게 요구할 사항들을 적어보세요!"
+              multiline
+            ></TextInput>
+            <Pressable style={{ alignSelf: "flex-end" }}>
+              <Entypo
+                name="arrow-with-circle-up"
+                size={24}
+                color={ASSETS.main}
+              />
+            </Pressable>
+          </View>
+
+          <BottomSheetModal ref={bottomSheetModalRef} snapPoints={snapPoints}>
+            <BottomSheetView style={[style["bottom-sheet-view"]]}>
+              <TouchableOpacity
+                style={style["bottom-sheet-button"]}
+                onPress={() => openCamera()}
+              >
+                <Feather name="camera" size={width * 0.2} color="black" />
+                <Text>사진을 찍어보세요!</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={style["bottom-sheet-button"]}
+                onPress={() => {
+                  openImageLibrary();
+                }}
+              >
+                <FontAwesome name="photo" size={width * 0.2} color="black" />
+                <Text>사진을 선택하세요!</Text>
+              </TouchableOpacity>
+            </BottomSheetView>
+          </BottomSheetModal>
+        </KeyboardAvoidingView>
+      </Pressable>
     </BottomSheetModalProvider>
   );
 }
--- screens/upload/style.ts
+++ screens/upload/style.ts
@@ -4,41 +4,52 @@ import {
   TextStyle,
   ViewStyle,
   Dimensions,
+  ImageStyle,
 } from "react-native";
 import { ASSETS } from "../../assets/assets";
 
 type UploadStyleKey =
   | "container"
-  | "upload-button"
+  | "upload-preview"
+  | "upload-preview-container"
   | "upload-button-text"
   | "bottom-sheet-view"
-  | "bottom-sheet-button";
+  | "bottom-sheet-button"
+  | "image-preview"
+  | "image-prompt"
+  | "image-prompt-container"
+  | "image-prompt-submit";
 
-export const UploadStyle: Record<
-  UploadStyleKey,
-  StyleProp<ViewStyle | TextStyle>
-> = StyleSheet.create({
+const UPLOAD_IMAGE_AR = 4 / 3;
+
+export const UploadStyle = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    width: "100%",
+    justifyContent: "space-between",
   },
-  "upload-button": {
-    width: "70%",
-    aspectRatio: 1,
-    backgroundColor: ASSETS.sub,
+  "upload-preview": {
+    width: "90%",
+    aspectRatio: UPLOAD_IMAGE_AR,
+    backgroundColor: ASSETS.pale,
     borderRadius: 20,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 1,
     },
     shadowOpacity: 0.23,
     shadowRadius: 2.62,
 
-    elevation: 4,
+    elevation: 1,
     alignItems: "center",
     justifyContent: "center",
+    marginTop: Dimensions.get("screen").height * 0.14,
+  },
+  "upload-preview-container": {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
   },
   "upload-button-text": {
     color: ASSETS.main,
@@ -72,6 +83,30 @@ export const UploadStyle: Record<
     justifyContent: "center",
     alignItems: "center",
   },
-});
+
+  "image-preview": {
+    width: "100%",
+    aspectRatio: UPLOAD_IMAGE_AR,
+    borderRadius: 10,
+  },
+  "image-prompt-container": {
+    width: "100%",
+    borderColor: ASSETS.input,
+    borderWidth: 0.3,
+    padding: 14,
+    borderRadius: 10,
+    flexDirection: "row",
+    maxHeight: 100,
+  },
+  "image-prompt": {
+    flex: 9,
+
+    height: "100%",
+  },
+  "image-prompt-submit": { alignSelf: "flex-end" },
+}) satisfies Record<
+  UploadStyleKey,
+  StyleProp<ViewStyle | TextStyle | ImageStyle>
+>;
 
 export default UploadStyle;
