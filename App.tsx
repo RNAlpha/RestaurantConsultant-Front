6cbaa4298126e5c12e8562a0cfd20dcb91c6630d
Taehyeungkim <kth1837@snu.ac.kr>
2024-09-29 03:29:43 +0900

    chore: wrapped app with redux provider

--- App.tsx
+++ App.tsx
@@ -6,31 +6,35 @@ import SignInScreen from "./screens/signin";
 import UploadScreen from "./screens/upload";
 import { RootStackParamList } from "./screens/type";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import Camera from "./components/camera";
+
 import SignUpScreen from "@_screens/signup";
 import MainScreen from "@_screens/main";
 import ChatScreen from "@_screens/chat";
+import { Provider } from "react-redux";
+import store from "state/store";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <GestureHandlerRootView>
-      <AppFrame>
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="signin"
-            screenOptions={{ headerShown: false }}
-          >
-            <Stack.Screen name="signin" component={SignInScreen} />
-            <Stack.Screen name="upload" component={UploadScreen} />
-            <Stack.Screen name="signup" component={SignUpScreen} />
-            <Stack.Screen name="main" component={MainScreen} />
-            <Stack.Screen name="chat" component={ChatScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </AppFrame>
-    </GestureHandlerRootView>
+    <Provider store={store}>
+      <GestureHandlerRootView>
+        <AppFrame>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="signin"
+              screenOptions={{ headerShown: false }}
+            >
+              <Stack.Screen name="signin" component={SignInScreen} />
+              <Stack.Screen name="upload" component={UploadScreen} />
+              <Stack.Screen name="signup" component={SignUpScreen} />
+              <Stack.Screen name="main" component={MainScreen} />
+              <Stack.Screen name="chat" component={ChatScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </AppFrame>
+      </GestureHandlerRootView>
+    </Provider>
     // <View style={styles.container}>
   );
 }
