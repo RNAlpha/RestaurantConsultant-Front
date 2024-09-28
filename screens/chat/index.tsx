c4d28dccb8e40990235ffeeaff88be6f524e16dd
Taehyeungkim <kth1837@snu.ac.kr>
2024-09-29 01:41:22 +0900

    feat: added chat prompt component to chat screen

--- screens/chat/index.tsx
+++ screens/chat/index.tsx
@@ -1,14 +1,32 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "@_screens/type";
-import { Text } from "react-native";
+import { KeyboardAvoidingView, ScrollView } from "react-native";
 import { ContainerWithHeader } from "@_layout/index";
+import { Prompt } from "@_components/prompt";
+import { useState } from "react";
 
 export default function ChatScreen({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, "chat">) {
+  const [prompt, setPrompt] = useState<string>("");
+
   return (
     <ContainerWithHeader navigation={navigation}>
-      <Text>chat</Text>
+      <KeyboardAvoidingView
+        style={{
+          flex: 1,
+          justifyContent: "space-between",
+        }}
+        behavior="height"
+      >
+        <ScrollView></ScrollView>
+        <Prompt
+          prompt={prompt}
+          onChange={(prompt) => setPrompt(prompt)}
+          submit={(prompt) => {}}
+          placeholder="AI에게 물어보고 싶은 것을 적어보세요!"
+        ></Prompt>
+      </KeyboardAvoidingView>
     </ContainerWithHeader>
   );
 }
