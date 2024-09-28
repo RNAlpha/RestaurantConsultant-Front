e00b4ba5a86f9300b65b7d5b07f4d9793828f765
Taehyeungkim <kth1837@snu.ac.kr>
2024-09-29 02:05:20 +0900

    chore: cleaned deprecated code

--- components/upload/index.tsx
+++ components/upload/index.tsx
@@ -1,32 +1,14 @@
-import {
-  View,
-  TextInput,
-  Pressable,
-  Animated,
-  Keyboard,
-  TouchableOpacity,
-  Text,
-} from "react-native";
-import Entypo from "@expo/vector-icons/Entypo";
-import {
-  ImagePromptStyle as IPS,
-  SubmitButtonStyle as SBS,
-  PromptPreviewStyle as PPS,
-} from "./style";
-import { ASSETS } from "@_assets/assets";
-import { ImageUploadPromptProps, PromptPreviewProps } from "./type";
-import {
-  useCallback,
-  useDeferredValue,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+import { SubmitButtonStyle as SBS, PromptPreviewStyle as PPS } from "./style";
+
+import { PromptPreviewProps } from "./type";
+
 import { FloatElementUI } from "@_components/UI/Float";
 
 export function SubmitButton() {
   return (
-    <FloatElementUI>
+    <FloatElementUI duration={1000}>
       <TouchableOpacity style={[SBS["submit-button"]]}>
         <Text style={SBS["submit-button-text"]}>AI 컨설팅 받기</Text>
       </TouchableOpacity>
@@ -36,7 +18,7 @@ export function SubmitButton() {
 
 export function PromptPreview({ prompt }: PromptPreviewProps) {
   return (
-    <FloatElementUI>
+    <FloatElementUI duration={1000}>
       <TouchableOpacity style={PPS["prompt-preview"]}>
         <Text style={PPS["prompt-preview-text"]}>{prompt}</Text>
       </TouchableOpacity>
